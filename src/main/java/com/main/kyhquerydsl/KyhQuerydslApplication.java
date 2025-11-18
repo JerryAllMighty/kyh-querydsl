ae2236cb33ee8b43e8f80c3ea55443ad18042151
KimYoonMin <dbsalszz@naver.com>
2025-11-18 20:03:50 +0900

    메인 출발점 추가

---
+++ src/main/java/com/main/kyhquerydsl/KyhQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.main.kyhquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhQuerydslApplication.class, args);
+    }
+
+}
