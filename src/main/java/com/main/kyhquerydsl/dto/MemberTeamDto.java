51ee948ad77cab4b0dbfbcfd8d030dd220abdce9
KimYoonMin <dbsalszz@naver.com>
2025-11-19 22:03:13 +0900

    Member Team 조회 DTO 추가

---
+++ src/main/java/com/main/kyhquerydsl/dto/MemberTeamDto.java
@@ -0,0 +1,18 @@
+package com.main.kyhquerydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    @QueryProjection
+    public MemberTeamDto(Long memberId) {
+        this.memberId = memberId;
+    }
+}
